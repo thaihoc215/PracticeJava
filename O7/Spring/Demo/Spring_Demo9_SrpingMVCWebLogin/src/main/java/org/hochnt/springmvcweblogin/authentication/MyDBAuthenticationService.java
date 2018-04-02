5aa8478dafabd7eb1e4f5eb314169fa55fd5879e
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2018-04-03 01:43:35 +0700

    edit authentication srping web login

--- O7/Spring/Demo/Spring_Demo9_SrpingMVCWebLogin/src/main/java/org/hochnt/springmvcweblogin/authentication/MyDBAuthenticationService.java
+++ O7/Spring/Demo/Spring_Demo9_SrpingMVCWebLogin/src/main/java/org/hochnt/springmvcweblogin/authentication/MyDBAuthenticationService.java
@@ -40,9 +40,18 @@ public class MyDBAuthenticationService implements UserDetailsService {
                 grantList.add(authority);
             }
         }        
-         
-        UserDetails userDetails = (UserDetails) new User(userInfo.getUserName(), //
-                userInfo.getPassword(),grantList);
+       /*
+        * In spring-security-core:5.0.0.RC1, the default PasswordEncoder is built as a 
+        * DelegatingPasswordEncoder. When you store the users in memory, 
+        * you are providing the passwords in plain text and when trying to retrieve the encoder from the DelegatingPasswordEncoder 
+        * to validate the password it can't find one that matches the way in which these passwords were stored.
+        * https://spring.io/blog/2017/11/01/spring-security-5-0-0-rc1-released#password-encoding
+        * https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
+        * */  
+        UserDetails userDetails = User.withDefaultPasswordEncoder().username(userInfo.getUserName()).
+        		password(userInfo.getPassword()).roles(roles.toArray(new String[0])).build();
+//        UserDetails userDetails = (UserDetails) new User(userInfo.getUserName(), //
+//                userInfo.getPassword(),grantList);
  
         return userDetails;
     }
